eeb9bab3470fa012c27fb82b8c34954a8037b26f
Rihanna P <114884466+RihannaP@users.noreply.github.com>
2025-10-23 20:45:51 +0100

    Implement Dom Obj

---
+++ frontend/chat.js
@@ -0,0 +1,7 @@
+const chatBox = document.querySelector("#chat-box");
+const form = document.querySelector("#message-form");
+const authorInput = document.querySelector("#author");
+const textInput = document.querySelector("#text");
+
+const backendUrl = "http://127.0.0.1:3000/messages";
+
