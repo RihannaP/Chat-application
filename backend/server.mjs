2d392fd1e7b4c014d62bfc1af353aca2935d9f97
Rihanna P <114884466+RihannaP@users.noreply.github.com>
2025-10-23 20:14:29 +0100

    Implement post method

--- backend/server.mjs
+++ backend/server.mjs
@@ -11,33 +11,23 @@ app.get("/messages", (req, res) => {
   res.send(messages);
 });
 
-// app.post("/", (req, res) => {
-//   const bodyBytes = [];
-//   req.on("data", chunk => bodyBytes.push(...chunk));
-//   req.on("end", () => {
-//     const bodyString = String.fromCharCode(...bodyBytes);
-//     let body;
-//     try {
-//       body = JSON.parse(bodyString);
-//     } catch (error) {
-//       console.error(`Failed to parse body ${bodyString} as JSON: ${error}`);
-//       res.status(400).send("Expected body to be JSON.");
-//       return;
-//     }
-//     if (typeof body != "object" || !("quote" in body) || !("author" in body)||
-//       !body.quote.trim() ||
-//       !body.author.trim()) {
-//       console.error(`Failed to extract quote and author from post body: ${bodyString}`);
-//       res.status(400).send("Expected body to be a JSON object containing keys quote and author.");
-//       return;
-//     }
-//     quotes.push({
-//       quote: body.quote,
-//       author: body.author,
-//     });
-//     res.send("ok");
-//   });
-// });
+app.post("/messages", (req, res) => {
+  const { text, author } = req.body;
+  if (!text?.trim() || !author?.trim()) {
+    res.status(400).send("Message text and author are required.");
+  return;
+    }
+
+    const message = {
+    id: messages.length + 1,
+    text: text.trim(),
+    author: author.trim(),
+  };
+
+  messages.push(message);
+  res.json(message);
+});
+
 
 // app.listen(port, () => {
 //   console.error(`Quote server listening on port ${port}`);
