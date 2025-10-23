a0e97543a29d767ad784480761370d16dbc1f073
Rihanna P <114884466+RihannaP@users.noreply.github.com>
2025-10-23 20:51:19 +0100

    Implement fetchMessages function

--- frontend/chat.js
+++ frontend/chat.js
@@ -5,3 +5,27 @@ const textInput = document.querySelector("#text");
 
 const backendUrl = "http://127.0.0.1:3000/messages";
 
+async function fetchMessages() {
+  const res = await fetch(backendUrl);
+  const messages = await response.text();
+
+  chatBox.textContent = ""; 
+  messages.forEach(msg => {
+    const div = document.createElement("div");
+    div.textContent = `${msg.author}: ${msg.text}`;
+    chatBox.appendChild(div);
+  });
+}
+
+// async function fetchQuote() {
+//   try {
+//     const response = await fetch(backendUrl);
+//     const text = await response.text();
+//     const [_, quote, author] = text.match(/"(.*)"\s*-(.*)/) || [];
+//     quoteLine.textContent = quote?.trim() || text;
+//     authorLine.textContent = author?.trim() || "";
+//   } catch {
+//     quoteLine.textContent = "Error fetching quote.";
+//     authorLine.textContent = "";
+//   }
+// }
